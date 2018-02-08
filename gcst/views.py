bb02b6695fe8cf081aa1ce6be0223b0bdec9ad61
john <john@saponara.net>
2018-02-07 21:01:39 -0500

    mere whitespace edit.

--- gcst/views.py
+++ gcst/views.py
@@ -9,22 +9,22 @@ from gcst.locate import find
 from gcst.gcst import fcstgfx
 
 def index(request):
-	#latest= Fcst.objects.all()[:5]
-	#t = loader.get_template('gcst/index.html')
-	#c = Context({ 'latest': latest, })
-	#return HttpResponse(t.render(c))
-	# OR
-	#return RequestContext( 'gcst/index.html' )
-	data={}
-	loc=request.GET.get('loc')
-	if loc:
-		data=find(loc)
-		if 'errmsg' not in data:
-			fcstdata=fcstgfx(data)
-			data.update(fcstdata)
-	return render_to_response(
-		'gcst/index.html',
-		data,
-		context_instance=RequestContext(request)  # arg needed for staticfiles to load
-		)
+    #latest= Fcst.objects.all()[:5]
+    #t = loader.get_template('gcst/index.html')
+    #c = Context({ 'latest': latest, })
+    #return HttpResponse(t.render(c))
+    # OR
+    #return RequestContext( 'gcst/index.html' )
+    data={}
+    loc=request.GET.get('loc')
+    if loc:
+        data=find(loc)
+        if 'errmsg' not in data:
+            fcstdata=fcstgfx(data)
+            data.update(fcstdata)
+    return render_to_response(
+        'gcst/index.html',
+        data,
+        context_instance=RequestContext(request)  # arg needed for staticfiles to load
+        )
 
